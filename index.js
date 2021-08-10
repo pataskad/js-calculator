0c9ac3ee6e5d7f9187df511c4105f6df07ca5c84
Dallas Pataska <pataskad@gmail.com>
2021-08-10 13:57:22 -0500

    simple arithmetic functions added, additional HTML skeleton framework

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 'use strict';
+
+
+// arithmetic functions
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
