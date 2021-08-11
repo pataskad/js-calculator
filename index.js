09244a9c70c63bb4b085a61044c9451ba993687b
Dallas Pataska <pataskad@gmail.com>
2021-08-11 13:56:06 -0500

    operator function, may need to be refactored for speed later.  Currently functioning for readability

--- index.js
+++ index.js
@@ -17,4 +17,13 @@ function divide(a, b) {
 
 function operate(a, b, operator) {
     // depending on operator, call necessary function
+    if (operator === '+') {
+        return add(a, b);
+    } else if (operator === '-') {
+        return subtract(a, b);
+    } else if (operator === '*') {
+        return multiply(a, b);
+    } else if (operator === '/') {
+        return divide(a, b);
+    }
 }
